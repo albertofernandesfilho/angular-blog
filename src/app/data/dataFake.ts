56c0b84b900a573439ab302c947e7f8b17d8c909
albfernandes <albfernandes@hotmail.com>
2023-09-27 17:42:33 -0300

    New Page

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,9 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photo":"https://fisionet.com.br/thumb/img/galery/1456323707lampada-mosquito.jpg__w520_h350",
+    "title":"Pensamento do Dia",
+    "description": "Imagine uma nova história para sua vida e acredite nela."
+  }
+
+ ]
---
+++ src/app/pages/content/content.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from 'src/app/data/dataFake';
+
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+
+
+export class ContentComponent implements OnInit {
+contentDescription: string= ""
+contentTitle: string = ""
+contentPhoto:string=""
+private id:string | null="0"
+
+
+  constructor(
+    private route:ActivatedRoute
+
+  ) {}
+
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(value => this.id = value.get("id"))
+
+    this.setValuesToComponent(this.id)
+    
+  }
+
+setValuesToComponent(id:string | null){
+  const result = dataFake.filter(article=>article.id == id)[0]
+  this.contentTitle = result.title
+  this.contentDescription = result.description
+  this.contentPhoto = result.photo
+}
+
+
+
+}
